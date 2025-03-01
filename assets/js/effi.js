ecb1e7fc01ea253ece1d4633bc8565cbd4e723c5
mohitgoyat525 <montyits71@gmail.com>
2025-03-01 11:31:21 +0530

    add-changes

--- assets/js/effi.js
+++ assets/js/effi.js
@@ -1,6 +1,5 @@
 const timeLine = gsap.timeline();
 
-
 gsap.set("#content-one", { opacity: 1 });
 gsap.set("#content-two", { opacity: 0 });
 gsap.set("#content-three", { opacity: 0 });
@@ -11,18 +10,25 @@ gsap.set("#img-one", { opacity: 1 });
 gsap.set("#img-two", { opacity: 0 });
 gsap.set("#effi-content", { opacity: 1 });
 gsap.set("#effi-content-two", { opacity: 0 });
-
+gsap.set("#content-counter", { opacity: 1 }); 
 
 const updateCounter = (value) => {
-  gsap.set("#content-counter", { innerHTML: value });
+  gsap.to("#content-counter", {
+    opacity: 0, // Fade out
+    duration: 0.3,
+    onComplete: () => {
+      gsap.set("#content-counter", { innerHTML: value });
+      gsap.to("#content-counter", { opacity: 1, duration: 0.3 }); 
+    },
+  });
 };
 
-// Timeline animation sequence
+
 timeLine
-    .call(updateCounter, ["1/6"])
+  .call(updateCounter, ["1/6"])
   .to("#content-one", { opacity: 0, duration: 1 })
   .to("#content-two", { opacity: 1, duration: 1 }, "-=0.5")
-  .call(updateCounter, ["2/6"], "-=1") 
+  .call(updateCounter, ["2/6"], "-=1")
 
   .to("#content-two", { opacity: 0, duration: 1 })
   .to("#content-three", { opacity: 1, duration: 1 }, "-=0.5")
@@ -41,10 +47,11 @@ timeLine
   .call(updateCounter, ["6/6"], "-=1")
 
   .to("#img-one", { opacity: 0, duration: 1 })
-    .to("#img-two", { opacity: 1, duration: 1 }, "-=0.5")
+  .to("#img-two", { opacity: 1, duration: 1 }, "-=0.5")
 
   .to("#effi-content", { opacity: 0, duration: 1 })
   .to("#effi-content-two", { opacity: 1, duration: 1 }, "-=0.5");
+
 
 ScrollTrigger.create({
   trigger: ".main-section",
