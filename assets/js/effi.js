114d38994dcd91ab1381bd746d75eb6bed17ec0b
mohitgoyat525 <montyits71@gmail.com>
2025-03-01 10:59:15 +0530

    add-new-section

---
+++ assets/js/effi.js
@@ -0,0 +1,56 @@
+const timeLine = gsap.timeline();
+
+
+gsap.set("#content-one", { opacity: 1 });
+gsap.set("#content-two", { opacity: 0 });
+gsap.set("#content-three", { opacity: 0 });
+gsap.set("#content-four", { opacity: 0 });
+gsap.set("#content-five", { opacity: 0 });
+gsap.set("#content-six", { opacity: 0 });
+gsap.set("#img-one", { opacity: 1 });
+gsap.set("#img-two", { opacity: 0 });
+gsap.set("#effi-content", { opacity: 1 });
+gsap.set("#effi-content-two", { opacity: 0 });
+
+
+const updateCounter = (value) => {
+  gsap.set("#content-counter", { innerHTML: value });
+};
+
+// Timeline animation sequence
+timeLine
+    .call(updateCounter, ["1/6"])
+  .to("#content-one", { opacity: 0, duration: 1 })
+  .to("#content-two", { opacity: 1, duration: 1 }, "-=0.5")
+  .call(updateCounter, ["2/6"], "-=1") 
+
+  .to("#content-two", { opacity: 0, duration: 1 })
+  .to("#content-three", { opacity: 1, duration: 1 }, "-=0.5")
+  .call(updateCounter, ["3/6"], "-=1")
+
+  .to("#content-three", { opacity: 0, duration: 1 })
+  .to("#content-four", { opacity: 1, duration: 1 }, "-=0.5")
+  .call(updateCounter, ["4/6"], "-=1")
+
+  .to("#content-four", { opacity: 0, duration: 1 })
+  .to("#content-five", { opacity: 1, duration: 1 }, "-=0.5")
+  .call(updateCounter, ["5/6"], "-=1")
+
+  .to("#content-five", { opacity: 0, duration: 1 })
+  .to("#content-six", { opacity: 1, duration: 1 }, "-=0.5")
+  .call(updateCounter, ["6/6"], "-=1")
+
+  .to("#img-one", { opacity: 0, duration: 1 })
+    .to("#img-two", { opacity: 1, duration: 1 }, "-=0.5")
+
+  .to("#effi-content", { opacity: 0, duration: 1 })
+  .to("#effi-content-two", { opacity: 1, duration: 1 }, "-=0.5");
+
+ScrollTrigger.create({
+  trigger: ".main-section",
+  start: "top top",
+  end: "300%",
+  pin: true,
+  animation: timeLine,
+  scrub: 2,
+});
